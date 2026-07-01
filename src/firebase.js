310d52d4216cae046d15d99940feb837ea4fecce
harijanaanesh026-dot <harijanaanesh026@gmail.com>
2026-07-01 10:41:45 +0530

    Modify Firebase config and add Google auth functions
    Updated Firebase configuration and added Google authentication methods.

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,34 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app"
+import { 
+  getAuth, 
+  GoogleAuthProvider, 
+  signInWithRedirect, 
+  getRedirectResult 
+} from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9BfrAh8djKof1Bu6FLG0Fz7X1ONCdm6g",
-    authDomain: "crickclash-d30fe.firebaseapp.com",
-      databaseURL: "https://crickclash-d30fe-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "crickclash-d30fe",
-          storageBucket: "crickclash-d30fe.firebasestorage.app",
-            messagingSenderId: "595133866613",
-              appId: "1:595133866613:web:dda3f0509462310cb74e3c"
-              };
+  apiKey: "AIzaSyD8vIrNAOqVF1dV7JkEWNEoBvC",
+  authDomain: "crickclash-d50fe.firebaseapp.com",
+  databaseURL: "https://crickclash-d50fe-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crickclash-d50fe",
+  storageBucket: "crickclash-d50fe.firebasestorage.app",
+  messagingSenderId: "999138800615",
+  appId: "1:999138800615:web:d68150904231b0b7a5e1c"
+}
 
-              const app = initializeApp(firebaseConfig);
-              export const auth = getAuth(app);
-              export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig)
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const googleProvider = new GoogleAuthProvider()
+
+// LOGIN FUNCTION - IDHI BUTTON KI VADU
+export const signInWithGoogle = () => {
+  signInWithRedirect(auth, googleProvider)
+}
+
+// RESULT CHECK FUNCTION - APP.JS LO VADU
+export const handleRedirectResult = () => {
+  return getRedirectResult(auth)
+}
