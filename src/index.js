6352e7ce9e9192a45fbc56aee1f5e1e1344fa37d
harijanaanesh026-dot <harijanaanesh026@gmail.com>
2026-07-09 18:39:34 +0530

    Remove unused index.css import
    Removed unused index.css import from index.js.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';  // <-- Iddhi add cheyi
-import './App.css';    // <-- Iddhi add cheyi
+import './App.css';  // only this one
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
