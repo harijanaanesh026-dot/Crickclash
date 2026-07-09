cd6ec60368be493a75f31fc28387732dedc4b449
harijanaanesh026-dot <harijanaanesh026@gmail.com>
2026-07-09 18:23:38 +0530

    Add CSS imports and fix rendering indentation
    Added CSS imports for styling and fixed indentation.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';  // <-- Iddhi add cheyi
+import './App.css';    // <-- Iddhi add cheyi
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
-        </React.StrictMode>
-        );
+    <App />
+  </React.StrictMode>
+);
